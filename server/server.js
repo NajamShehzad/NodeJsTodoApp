8f9d9b0491a400ee8e6c24626fa879435bcb9f68
NajamShehzad <najambutt195@gmail.com>
2018-08-07 16:15:18 +0500

    Adding logout Route to remove token

--- server/server.js
+++ server/server.js
@@ -173,7 +173,7 @@ app.post('/users/login', (req, res) => {
 app.delete('/users/me/token',authenticate,(req,res) => {
     // console.log(req.user,req.token);
     req.user.removeToken(req.token).then(()=> {
-        res.status(200).send('Token Removed');
+        res.status(200).send('Token Removed'); 
     },() => {
         res.status(400).send();
     })
